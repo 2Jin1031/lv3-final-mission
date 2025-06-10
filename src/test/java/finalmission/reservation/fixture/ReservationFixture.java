07e2d8ca90c2bddf439d0236feeeb835cfbdaf5b
2jin2.1031 <2jin2.1031@gmail.com>
2025-06-10 16:06:00 +0900

    feat: 회의실 예약 전체 리스트 조회 기능 구현

--- src/test/java/finalmission/reservation/fixture/ReservationFixture.java
+++ src/test/java/finalmission/reservation/fixture/ReservationFixture.java
@@ -1,10 +1,17 @@
 package finalmission.reservation.fixture;
 
+import finalmission.reservation.Reservation;
 import finalmission.reservation.domain.dto.ReservationRequestDto;
+import finalmission.room.domain.Room;
+import finalmission.user.User;
 
 public class ReservationFixture {
 
     public static ReservationRequestDto createReservationRequestDtoDefaultByRoomId(Long roomId) {
         return new ReservationRequestDto("content1", roomId);
+    }
+
+    public static Reservation createReservationDefaultByRoomIdAndUserId(Room savedRoom, User savedMember) {
+        return new Reservation("contnent1", savedRoom, savedMember);
     }
 }
--- src/test/java/finalmission/reservation/service/ReservationServiceTest.java
+++ src/test/java/finalmission/reservation/service/ReservationServiceTest.java
@@ -1,14 +1,17 @@
 package finalmission.reservation.service;
 
+import finalmission.reservation.Reservation;
 import finalmission.reservation.domain.dto.ReservationRequestDto;
 import finalmission.reservation.domain.dto.ReservationResponseDto;
 import finalmission.reservation.fixture.ReservationFixture;
+import finalmission.reservation.repository.ReservationRepository;
 import finalmission.room.domain.Room;
 import finalmission.room.fixture.RoomFixture;
 import finalmission.room.repository.RoomRepository;
 import finalmission.user.User;
 import finalmission.user.fixture.UserFixture;
 import finalmission.user.repository.UserRepository;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -27,12 +30,15 @@ class ReservationServiceTest {
     @Autowired
     private ReservationService reservationService;
     @Autowired
+    private ReservationRepository reservationRepository;
+    @Autowired
     private RoomRepository roomRepository;
     @Autowired
     private UserRepository userRepository;
 
     private Room savedRoom;
     private User savedMember;
+
 
     @BeforeEach
     void beforeEach() {
@@ -58,6 +64,37 @@ class ReservationServiceTest {
 
             // then
             Assertions.assertThat(responseDto.roomResponseDto().id()).isEqualTo(savedRoom.getId());
+        }
+    }
+
+    @Nested
+    @DisplayName("회의실 ID로 예약 전체 목록 조회")
+    class findAllByRoomId {
+
+        @DisplayName("요청 회의실 ID로 예약 목록이 없더라도 예외 없이 List.of()를 반환한다.")
+        @Test
+        void findAllByRoomId_success_byNoData() {
+            // when
+            List<ReservationResponseDto> responseDto = reservationService.findAllByRoomId(savedRoom.getId());
+
+            // then
+            Assertions.assertThat(responseDto).hasSize(0);
+        }
+
+        @DisplayName("요청 회의실 ID로 예약 목록을 전체 조회할 수 있다.")
+        @Test
+        void findAllByRoomId_success() {
+            // given
+            Reservation reservation1 = ReservationFixture.createReservationDefaultByRoomIdAndUserId(savedRoom, savedMember);
+            reservationRepository.save(reservation1);
+            Reservation reservation2 = ReservationFixture.createReservationDefaultByRoomIdAndUserId(savedRoom, savedMember);
+            reservationRepository.save(reservation2);
+
+            // when
+            List<ReservationResponseDto> responseDto = reservationService.findAllByRoomId(savedRoom.getId());
+
+            // then
+            Assertions.assertThat(responseDto).hasSize(2);
         }
     }
 }
