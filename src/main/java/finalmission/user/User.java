4faf452022c79b6c2a00775371b664243d7a6523
2jin2.1031 <2jin2.1031@gmail.com>
2025-06-10 13:40:38 +0900

    feat: 도메인 객체 생성

---
+++ src/main/java/finalmission/user/User.java
@@ -0,0 +1,23 @@
+package finalmission.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity(name = "Users")
+@Getter
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
