59756f171738cb18d9e1d9b9eae848bd7f52927d
2jin2.1031 <2jin2.1031@gmail.com>
2025-06-10 13:40:38 +0900

    feat: 도메인 객체 생성

---
+++ src/main/java/finalmission/reservation/Reservation.java
@@ -0,0 +1,26 @@
+package finalmission.reservation;
+
+import finalmission.room.Room;
+import finalmission.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Reservation {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    private Room room;
+
+    @ManyToOne
+    private User user;
+}
---
+++ src/main/java/finalmission/room/Room.java
@@ -0,0 +1,18 @@
+package finalmission.room;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+}
